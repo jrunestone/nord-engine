5cfbb7f337fab29ce08d892b2cc810e08ffc03c1
Johan Runsten <johanrunsten86@gmail.com>
2024-01-04 10:51:55 +0100

    Made ui work

--- src/Nord.Engine/Ecs/Systems/DefaultUiRenderSystem.cs
+++ src/Nord.Engine/Ecs/Systems/DefaultUiRenderSystem.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Arch.Core;
 using Nord.Engine.Core;
 using Nord.Engine.Ecs.Components;
 using SFML.Graphics;
+using SFML.Window;
 using UltralightNet;
 using UltralightNet.AppCore;
 using View = UltralightNet.View;
@@ -32,8 +34,12 @@ public class DefaultUiRenderSystem : SystemBase
         var cfg = new ULConfig();
         _renderer = ULPlatform.CreateRenderer(cfg);
         _view = _renderer.CreateView(1024, 768);
-        _view.OnFinishLoading += (_, _, _) => _isLoaded = true;
-        _view.URL = "https://ultralig.ht";
+        _view.OnFinishLoading += (_, _, _) =>
+        {
+            _isLoaded = true;
+            _view!.Focus();
+        };
+        _view.HTML = "<html><head><style type='text/css'>*,html,body{margin:0;padding:0;}</style></head><body><input type='text'/></body></html>";
     }
 
     public override void Update(float dt)
@@ -49,18 +55,28 @@ public class DefaultUiRenderSystem : SystemBase
         World.Query(in _query, (ref UiComponent ui) =>
         {
             var bmp = _view!.Surface!.Value.Bitmap;
-            var bytes = new byte[bmp.Width * bmp.Height];
+            var bytes = new byte[bmp.Width * bmp.Height * bmp.Bpp];
             if (ui.Sprite == null)
             {
-                ui.Sprite = new Sprite(new Texture(bmp.Width, bmp.Height));
+                ui.Sprite = new Sprite(new Texture(_view.Width, _view.Height));
             }
             unsafe
             {
-                // too large texture??
                 Marshal.Copy((IntPtr)bmp.LockPixels(), bytes, 0, bytes.Length);
             }
             bmp.UnlockPixels();
-            ui.Sprite!.Texture.Update(bytes, bmp.Width, bmp.Height, 0, 0);
+            ui.Sprite!.Texture.Update(bytes);
+
+            if (Mouse.IsButtonPressed(Mouse.Button.Left))
+            {
+                _view!.FireMouseEvent(new ULMouseEvent(){Button = ULMouseEventButton.Left, Type = ULMouseEventType.MouseDown, X = 1, Y = 1});
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+            {
+                _view!.FireKeyEvent(ULKeyEvent.Create(ULKeyEventType.RawKeyDown, ULKeyEventModifiers.ShiftKey, 65, 0, "A", "a", false, false, false));
+                _view!.FireKeyEvent(ULKeyEvent.Create(ULKeyEventType.Char, ULKeyEventModifiers.ShiftKey, 65, 0, "A", "a", false, false, false));
+            }
             _renderTarget.RenderTexture?.Draw(ui.Sprite);
         });
     }
